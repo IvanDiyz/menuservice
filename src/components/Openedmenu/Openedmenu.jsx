cc79a9e8949ba31f87b9c0dcfed186b6815e3779
anatoliimaslivets <anatolii.maslivets@sonect.ch>
2023-12-21 03:23:03 +0200

    fix component path #101;

--- src/components/Openedmenu/Openedmenu.jsx
+++ src/components/Openedmenu/Openedmenu.jsx
@@ -1,5 +1,5 @@
 import s from "./Openedmenu.module.scss";
-import Openedinfo from "@/components/Openedmenu/Openedinfo/Openedinfo";
+import Openedinfo from "@/components/Openedmenu/OpenedInfo/OpenedInfo";
 import Openedcontact from "@/components/Openedmenu/Openedcontact/Openedcontact";
 import OrderMethods from "@/components/ClientMain/OrderMethods/OrderMethods";
 import ServiceSheet from "@/components/ClientMain/ServiceSheet/ServiceSheet";
@@ -12,7 +12,7 @@ export default function Openedmenu() {
     <div className={s.openedMenu}>
       <Openedinfo name={name}/>
       <OrderMethods firstmethod={'В закладі'} lastmethod={'Із собою'}/>
-      {menus ? <ServiceSheet menus={menus}/> : 'Loading'} 
+      {menus ? <ServiceSheet menus={menus}/> : 'Loading'}
       <Openedcontact />
     </div>
   );
