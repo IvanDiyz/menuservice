eea78bdade4a826e23d4cf3eae17bdd13c2d0230
anatoliimaslivets <anatolii.maslivets@sonect.ch>
2023-12-21 03:15:53 +0200

    comment unused components #101;

--- src/app/[idvenue]/[idtable]/(result)/result/page.jsx
+++ src/app/[idvenue]/[idtable]/(result)/result/page.jsx
@@ -1,6 +1,6 @@
-import Filtres from "@/components/Filtres/Filtres";
+// import Filtres from "@/components/Filtres/Filtres";
 import Pageresult from "@/components/Pageresult/Pageresult";
-import Openedmenu from "@/components/Openedmenu/Openedmenu";
+// import Openedmenu from "@/components/Openedmenu/Openedmenu";
 
 
 const result = () => {
